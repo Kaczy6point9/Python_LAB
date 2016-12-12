9db4962d6632e651141522ce57a289eba6edfadf
Kaczy6point9 <kaczytkd@gmail.com>
2016-12-12 01:08:23 +0100

    Add files via upload

--- exercises/rozwiazania/weasel.py
+++ exercises/rozwiazania/weasel.py
@@ -5,28 +5,34 @@ def randomString(size):
     return ''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(size)) 
 
 def stringmutation(sentence):
-	new_senetence = list(sentence)
-	index = random.randint(0, 27)
-	new_senetence[index] = random.choice(string.ascii_uppercase + ' ')
-	return ''.join(new_senetence)
+	new_sentence = list(sentence)
+	mutation_prob = 0.09
+	##index = random.randrange(len(sentence))
+	for x in range(len(sentence)):
+		if random.random() < mutation_prob:
+			new_sentence[x] = random.choice(string.ascii_uppercase + ' ')
+	
+	return ''.join(new_sentence)
 
-def sen_score(score_sentence, sentences):
-	score = len(score_sentence)
+def sen_score(final_sentence, sentences):
 	max_score = 0
 	best_sentence = sentences[0]
+	
 
 	for x in range(len(sentences)):
-		sentence_score = 0
 		pom1 = sentences[x]
-		if pom1 == score_sentence:
+		if pom1 == final_sentence:
 			return pom1
 		else:
-			for y in range(len(pom1)):
-				if pom1[y] == score_sentence[y]:
+			sentence_score = 0
+			for y in range (len(final_sentence)):
+				if pom1[y] == final_sentence[y]:
 						sentence_score += 1
-				if sentence_score > max_score:
-					best_sentence = pom1
-			
+			if sentence_score > max_score:
+				best_sentence = pom1
+	
+	print(sentence_score)
+	print(best_sentence)		
 	return best_sentence
 
 
@@ -37,12 +43,25 @@ def weasel(score, sentence):
 			sentences.append(sentence)
 	
 		mutations = []
-
+		ile = 0
 		for x in range(len(sentences)):
 			mutations.append(stringmutation(sentences[x]))
 
-	 
+		best_sentence = sen_score(score, mutations)
+		while best_sentence != score:
+			del sentences[:]
+			del mutations[:]
+			for _ in range(100):
+				sentences.append(best_sentence)
 
-sentence = randomString(28)
-print(weasel('METHINKS IT IS LIKE A WEASEL', sentence))
+			for x in range(len(sentences)):
+				mutations.append(stringmutation(sentences[x]))
+			best_sentence = sen_score(score, mutations)
+			ile += 1
+			print(ile)
+		return best_sentence
+
+score ='METHINKS IT IS LIKE A WEASEL'
+sentence = randomString(len(score))
+print(weasel(score, sentence))
 
