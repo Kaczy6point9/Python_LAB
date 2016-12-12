ee72d5ce2c6ae2c00d7d853b47651303651cc32e
Kaczy6point9 <kaczytkd@gmail.com>
2016-12-12 10:31:48 +0100

    Update weasel.py

--- exercises/rozwiazania/weasel.py
+++ exercises/rozwiazania/weasel.py
@@ -1,67 +1,63 @@
 import random
 import string
+import argparse
 
 def randomString(size):
     return ''.join(random.choice(string.ascii_uppercase + ' ') for _ in range(size)) 
 
 def stringmutation(sentence):
 	new_sentence = list(sentence)
-	mutation_prob = 0.09
-	##index = random.randrange(len(sentence))
-	for x in range(len(sentence)):
-		if random.random() < mutation_prob:
-			new_sentence[x] = random.choice(string.ascii_uppercase + ' ')
+	#mutation_prob = 0.05
+	index = random.randrange(len(sentence))
+	##for x in range(len(sentence)):
+		##if random.random() < mutation_prob:
+	new_sentence[index] = random.choice(string.ascii_uppercase + ' ')
 	
 	return ''.join(new_sentence)
 
-def sen_score(final_sentence, sentences):
-	max_score = 0
-	best_sentence = sentences[0]
-	
 
-	for x in range(len(sentences)):
-		pom1 = sentences[x]
-		if pom1 == final_sentence:
-			return pom1
-		else:
-			sentence_score = 0
-			for y in range (len(final_sentence)):
-				if pom1[y] == final_sentence[y]:
-						sentence_score += 1
-			if sentence_score > max_score:
-				best_sentence = pom1
-	
-	print(sentence_score)
-	print(best_sentence)		
-	return best_sentence
+def sentence_score(sentence, final_sentence):
+	sen_score=0	
+	sen = list(sentence)
+	final = list(final_sentence)
+	for x in range(len(final)):
+		if sen[x] == final[x]:
+			sen_score += 1
+	return(sen_score)
 
 
-def weasel(score, sentence):
+def weasel(score, sentence, copies):
 		
 		sentences = []	
-		for _ in range(100):
-			sentences.append(sentence)
-	
 		mutations = []
 		ile = 0
-		for x in range(len(sentences)):
-			mutations.append(stringmutation(sentences[x]))
-
-		best_sentence = sen_score(score, mutations)
+		best_sentence = sentence
+		best_score = sentence_score(sentence, score)
 		while best_sentence != score:
 			del sentences[:]
 			del mutations[:]
-			for _ in range(100):
+			for _ in range(copies):
 				sentences.append(best_sentence)
 
 			for x in range(len(sentences)):
 				mutations.append(stringmutation(sentences[x]))
-			best_sentence = sen_score(score, mutations)
+			for x in range(len(mutations)):
+				if sentence_score(mutations[x], score) > best_score:
+					best_sentence = mutations[x]
+					best_score = sentence_score(mutations[x], score)
 			ile += 1
 			print(ile)
+			print(best_sentence)
 		return best_sentence
 
-score ='METHINKS IT IS LIKE A WEASEL'
+
+parser = argparse.ArgumentParser(description='Argumenty wchodzace do symulacji')
+parser.add_argument('--sentence', '-s', type=str, default='METHINKS IT IS LIKE A WEASEL') 
+parser.add_argument('--copies', '-c', type=int, default=100)
+args = parser.parse_args()
+
+score =args.sentence
+copies=args.copies
 sentence = randomString(len(score))
-print(weasel(score, sentence))
+print(weasel(score, sentence, copies))
 
